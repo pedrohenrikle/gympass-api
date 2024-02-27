3586d3fe759485730cdb65fb8ac5df6608885ddd
Pedro Klein <pedro.klein.sl@gmail.com>
2024-02-27 15:16:33 -0300

    ci: improve error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error ', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // todo: here we should log to an external tool like DataDog/Sentry/NewRelic
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -20,7 +21,7 @@ export class RegisterUseCase {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('E-mail already exists')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({
