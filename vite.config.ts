f519c526ecbf91aad2431359687182fe9c54843c
Pedro Klein <pedro.klein.sl@gmail.com>
2024-03-05 14:41:43 -0300

    ci: add e2e and environment configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    environmentMatchGlobs: [['./src/http/controllers/**', './vitest-environment/prisma.ts']],
+    dir: 'src',
   },
 })
---
+++ vitest-environment/prisma.ts
@@ -0,0 +1,40 @@
+import { prisma } from '@/lib/prisma'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+import { Environment } from 'vitest'
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  console.log(url)
+
+  return url.toString()
+}
+
+export default <Environment>{
+  name: 'prisma',
+  transformMode: 'ssr',
+  async setup() {
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
+
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+
+        await prisma.$disconnect()
+      },
+    }
+  },
+}
