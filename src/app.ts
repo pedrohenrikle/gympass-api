d3b0f6b3ccef4944171790b7ebd0d298cc8c5432
Pedro Klein <pedro.klein.sl@gmail.com>
2024-03-06 13:37:03 -0300

    ci: add refreshJWT token

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,22 @@ import { env } from '@/env'
 import { usersRoutes } from '@/http/controllers/users/routes'
 import { gymsRoutes } from '@/http/controllers/gyms/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
